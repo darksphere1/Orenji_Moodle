c009f3f00ecff1bf2b18145415ec5728473ed3b7
Raphael Scalfoni <raphael_scalfoni@hotmail.com>
2022-06-27 11:45:12 -0300

    Update preinsc.sql

--- SQL/preinsc.sql
+++ SQL/preinsc.sql
@@ -1,1 +1,10 @@
-CREATE TABLE mdl_user_preinsc ( id int(11) NOT NULL, id_curso int(11) NOT NULL DEFAULT '0' COMMENT 'chave primaria dupla na validacao', cpf varchar(20) NOT NULL DEFAULT '0' COMMENT 'chave primaria dupla na validacao', nome varchar(1000) NOT NULL, email varchar(1000) NOT NULL, institution varchar(1000) NOT NULL, ambiente int(1) UNSIGNED ZEROFILL NOT NULL COMMENT 'valor padrao ''zero'' ', valid int(1) UNSIGNED ZEROFILL NOT NULL COMMENT 'valor padrao ''zero'' para user nao validado. update valor ''um'' para user validado', time varchar(10) NOT NULL ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE mdl_user_preinsc (
+  id int(11) NOT NULL
+  ,id_curso int(11) NOT NULL DEFAULT '0' COMMENT 'chave primaria dupla na validacao'
+  , cpf varchar(20) NOT NULL DEFAULT '0' COMMENT 'chave primaria dupla na validacao'
+  , nome varchar(1000) NOT NULL, email varchar(1000) NOT NULL
+  , institution varchar(1000) NOT NULL
+  , ambiente int(1) UNSIGNED ZEROFILL NOT NULL COMMENT 'valor padrao ''zero'' '
+  , valid int(1) UNSIGNED ZEROFILL NOT NULL COMMENT 'valor padrao ''zero'' para user nao validado. update valor ''um'' para user validado'
+  , time varchar(10) NOT NULL
+) 
